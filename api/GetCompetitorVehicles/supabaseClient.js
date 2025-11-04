c640e1d065041349a5ddc8eb2efd6c8e033d596e
HenriqueRibeiroVolvocars <henrique.baldim@volvocars.com>
2025-11-04 10:34:36 -0300

    att: api setting

--- api/GetCompetitorVehicles/supabaseClient.js
+++ api/GetCompetitorVehicles/supabaseClient.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const SUPABASE_URL = 'https://qmcjihjuxjnwlgmzdevl.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtY2ppaGp1eGpud2xnbXpkZXZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTA3NTQsImV4cCI6MjA2ODg2Njc1NH0.DVl9uph4wQUpL4Dvo2980Y-uYWc7m9dkw2K8bLAdPtU';
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error("Variáveis SUPABASE_URL e SUPABASE_KEY não configuradas!");
