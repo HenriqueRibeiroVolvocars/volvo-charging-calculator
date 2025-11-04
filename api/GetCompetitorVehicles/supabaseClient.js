a3b3ef81cf859faf2b19605aa73f4337bc1af9c4
HenriqueRibeiroVolvocars <henrique.baldim@volvocars.com>
2025-11-04 10:31:28 -0300

    att: api setting

---
+++ api/GetCompetitorVehicles/supabaseClient.js
@@ -0,0 +1,14 @@
+// backend/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  throw new Error("Variáveis SUPABASE_URL e SUPABASE_KEY não configuradas!");
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
